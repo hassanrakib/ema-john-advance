333e411040cccce1a034a333b19d1069e3c51a60
Rakib Hassan <rakiboe@gmail.com>
2021-10-12 18:57:55 +0600

    second commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBveg_limr31I2ShR3EZvQPP8qfoLVhr1A",
+    authDomain: "ema-john-firebase-practice.firebaseapp.com",
+    projectId: "ema-john-firebase-practice",
+    storageBucket: "ema-john-firebase-practice.appspot.com",
+    messagingSenderId: "896523887778",
+    appId: "1:896523887778:web:92efa23fe38b19922d23fb"
+};
+
+export default firebaseConfig;
